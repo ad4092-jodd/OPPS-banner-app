dbcfdf84079ecbe70cd979c7a49d14ae1ef581ac
ad4092-jodd <ad4092@srmist.edu.in>
2026-02-24 15:39:34 +0530

    Create oops banner app.java

---
+++ oops banner app.java
@@ -0,0 +1,10 @@
+public class OopsBanner {
+    public static void main(String[] args) {
+        System.out.println(" *** *** **** ****");
+        System.out.println("* * * * * * * ");
+        System.out.println("* * * * **** *** ");
+        System.out.println("* * * * * *");
+        System.out.println(" *** *** * **** ");
+    }
+}
+EOF
